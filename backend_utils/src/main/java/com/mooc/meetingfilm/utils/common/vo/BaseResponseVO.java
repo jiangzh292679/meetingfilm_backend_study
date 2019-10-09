74782e673a136f7f85155f63b3c327eda899e27d
jiangzh <jiangzh@qq.com>
2019-10-09 08:18:52 +0800

    公共工具模块创建

---
+++ backend_utils/src/main/java/com/mooc/meetingfilm/utils/common/vo/BaseResponseVO.java
@@ -0,0 +1,14 @@
+package com.mooc.meetingfilm.utils.common.vo;
+
+/**
+ * @author : jiangzh
+ * @program : com.mooc.meetingfilm.utils.common.vo
+ * @description : 表现层公共返回
+ **/
+public class BaseResponseVO {
+
+    public String run (String message){
+        return "baseResponseVO : "+ message;
+    }
+
+}
