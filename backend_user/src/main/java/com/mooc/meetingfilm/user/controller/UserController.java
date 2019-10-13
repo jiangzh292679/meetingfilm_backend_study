813c6e2ecec44bf6f368d7f16a103e5cd937326b
jiangzh <jiangzh@qq.com>
2019-10-13 15:16:55 +0800

    用户登录信息验证实现

--- backend_user/src/main/java/com/mooc/meetingfilm/user/controller/UserController.java
+++ backend_user/src/main/java/com/mooc/meetingfilm/user/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.mooc.meetingfilm.user.controller;
 
 import com.mooc.meetingfilm.user.controller.vo.LoginReqVO;
+import com.mooc.meetingfilm.user.service.UserServiceAPI;
 import com.mooc.meetingfilm.utils.common.vo.BaseResponseVO;
 import com.mooc.meetingfilm.utils.exception.CommonServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,11 +22,17 @@ import java.util.Map;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    @Autowired
+    private UserServiceAPI serviceAPI;
+
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     public BaseResponseVO login(@RequestBody LoginReqVO reqVO) throws CommonServiceException {
 
         // 数据验证
         reqVO.checkParam();
+
+        // 验证用户名和密码是否正确
+        String userId = serviceAPI.checkUserLogin(reqVO.getUsername(),reqVO.getPassword());
 
         // randomKey  token
         Map<String,String> result = new HashMap<>();
