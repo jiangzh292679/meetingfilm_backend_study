78345ebca68bb897246e657a5db7ff9a47d3a8af
jiangzh <jiangzh@qq.com>
2019-10-14 22:55:53 +0800

    Eureka Client构建演示

--- backend_show/backend_show_consumer/src/main/java/com/mooc/meetingfilm/consumer/BackendShowConsumerApplication.java
+++ backend_show/backend_show_consumer/src/main/java/com/mooc/meetingfilm/consumer/BackendShowConsumerApplication.java
@@ -2,7 +2,11 @@ package com.mooc.meetingfilm.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient  // DiscoverClient可以集成大部分注册中心
+// @EnableEurekaClient  只对Eureka使用
 @SpringBootApplication
 public class BackendShowConsumerApplication {
 
