cdec452ae24da95a67ab446ebaab3c10fb0cfdff
jiangzh <jiangzh@qq.com>
2019-10-21 08:17:57 +0800

    播放厅模块基础环境构建

---
+++ backend_hall/src/main/java/com/mooc/meetingfilm/hall/dao/mapper/MoocFieldTMapper.java
@@ -0,0 +1,16 @@
+package com.mooc.meetingfilm.hall.dao.mapper;
+
+import com.mooc.meetingfilm.hall.dao.entity.MoocFieldT;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 放映场次表 Mapper 接口
+ * </p>
+ *
+ * @author jiangzh
+ * @since 2019-10-21
+ */
+public interface MoocFieldTMapper extends BaseMapper<MoocFieldT> {
+
+}
